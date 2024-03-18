e827db307631dbbbcd58b5aeb2aa95dd6f60a3ec
Daniel Alemán <dan13laleman14@gmail>
2024-03-17 21:10:49 -0300

    primera estructura

---
+++ app.js
@@ -0,0 +1,36 @@
+import express from "express"
+import handlebars from "express-handlebars"
+import { Server } from "socket.io"
+import mongoose from "mongoose"
+import __dirname from './utils.js';
+
+const app = express()
+const port = process.env.port || 8080
+const  server = app.listen(port, ()=> console.log("servidor operando en puerto", port))
+
+
+app.set('views',__dirname+'/views')
+app.set('view engine', 'handlebars')
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static(__dirname+'/public'))
+app.engine('handlebars', handlebars.engine())
+
+
+const connectMongoDB = async () => {
+    const DB_URL = "mongodb+srv://dan13l:dani06011998@cluster0.pm7efvk.mongodb.net/"
+    try{
+        await mongoose.connect(DB_URL)
+        console.log("mongodb conectado")
+    }catch(error){
+        console.error("No se conecto a mongo", error)
+        process.exit()
+    }
+    }
+    
+connectMongoDB()
+
+
+const io = new Server(server)
+
+
